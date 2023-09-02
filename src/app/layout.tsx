d481c98ee6ab9f3d450e1a89e2b3206ce1877ae1
Maged Ibrahim <magedibra@yahoo.com>
2023-09-02 16:31:45 +0300

    Change the font to lato

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import "./globals.scss";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/providers";
-import { Inter } from "next/font/google";
+import { Lato } from "next/font/google";
 import Navbar from "@/sections/Navbar/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const lato = Lato({ subsets: ["latin"], weight: ["400", "700"] });
 const title = "Magym";
 const description =
   "Welcome to magym, the ultimate online destination for your fitness goals. Whether you want to lose weight, gain muscle, or improve your health, we have the perfect exercises for you. Browse our website and discover how magym can help you transform your body and your life.";
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={lato.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
           <main>{children}</main>
