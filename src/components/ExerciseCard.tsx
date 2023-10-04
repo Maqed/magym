1ec6afa26bd9c3fc3128b7459265c3e5a52ae0a6
Maged Ibrahim <magedibra@yahoo.com>
2023-10-04 09:28:32 +0300

    Added badges to the card and added showed secondary muscles in the badges

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Dumbbell } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 import {
   Card,
   CardContent,
@@ -12,7 +13,6 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -25,16 +25,35 @@ function ExerciseCard({
   bodyPart,
   target,
   instructions,
+  secondaryMuscles,
 }: ExerciseType) {
   return (
     <Card className="w-[350px]">
       <CardHeader className="flex flex-col items-center justify-center">
         <CardTitle className="capitalize">{name}</CardTitle>
-        <CardDescription className="capitalize">{bodyPart}</CardDescription>
-        <CardDescription className="capitalize">{target}</CardDescription>
-        <CardDescription className="capitalize">
-          <Dumbbell className="inline mr-1 w-[1em] h-[1em]" /> {equipment}
-        </CardDescription>
+        <div className="flex flex-col flex-wrap items-center justify-between gap-3 py-2">
+          <div className="flex flex-wrap items-center justify-center gap-3">
+            <Badge className="capitalize">{bodyPart}</Badge>
+            <Badge className="capitalize">{target}</Badge>
+            {secondaryMuscles.map((muscle) => {
+              return (
+                <Badge
+                  className="capitalize"
+                  key={`${name}-secondary-muscle-${muscle}`}
+                >
+                  {muscle}
+                </Badge>
+              );
+            })}
+          </div>
+          <div className="flex flex-wrap items-center justify-center gap-3">
+            <Badge className="capitalize">
+              <Dumbbell className="inline mr-1 w-[1em] h-[1em]" /> {equipment}
+            </Badge>
+          </div>
+        </div>
+
+        <CardDescription className="capitalize"></CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col items-center justify-center">
         <Image width={200} height={200} src={gifUrl} alt={name} />
