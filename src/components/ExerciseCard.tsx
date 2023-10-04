046cfa689db9121c22e9b2db683582a8fbfe44dd
Maged Ibrahim <magedibra@yahoo.com>
2023-10-04 06:04:41 +0300

    Added bodyPart to ExerciseCard component

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -13,12 +13,14 @@ function ExerciseCard({
   equipment,
   gifUrl,
   name,
+  bodyPart,
   target,
 }: Partial<ExerciseType>) {
   return (
     <Card className="w-[350px]">
       <CardHeader className="flex flex-col items-center justify-center">
         <CardTitle className="capitalize">{name}</CardTitle>
+        <CardDescription className="capitalize">{bodyPart}</CardDescription>
         <CardDescription className="capitalize">{target}</CardDescription>
         <CardDescription className="capitalize">
           <Dumbbell className="inline mr-1 w-[1em] h-[1em]" /> {equipment}
