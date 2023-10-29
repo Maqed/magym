ba95772d8b2b8b3aef78b63d742148ded378aef9
Maged Ibrahim <magedibra@yahoo.com>
2023-10-29 08:45:57 +0200

    Fix Combobox bug

--- src/components/FilterComponents.tsx
+++ src/components/FilterComponents.tsx
@@ -2,7 +2,7 @@
 import React, { FormEvent, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Input } from "@/components/ui/input";
-import { BODY_PARTS, EXERCISE_EQUIPMENT } from "@/consts/gym";
+import { BODY_PARTS, EXERCISE_EQUIPMENTS } from "@/consts/gym";
 import { Check, ChevronsUpDown } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -73,39 +73,45 @@ function FilterComponents() {
               variant="outline"
               role="combobox"
               aria-expanded={isBodyPartComboboxOpen}
-              className="w-[200px] justify-between capitalize"
+              className="w-[200px] justify-between"
             >
-              {selectedBodyPart ? selectedBodyPart : "Select Body Part..."}
+              {selectedBodyPart
+                ? [{ value: "all", label: "All" }, ...BODY_PARTS].find(
+                    (bodyPart) => bodyPart.value === selectedBodyPart
+                  )?.label
+                : "Select Body Part..."}
               <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-[200px] p-0">
             <Command>
-              <CommandInput placeholder="Search Body Part..." />
-              <CommandEmpty>Body part wasn&apos;t found.</CommandEmpty>
+              <CommandInput placeholder="Search body part..." />
+              <CommandEmpty>Body part wasn&apos;t found</CommandEmpty>
               <CommandGroup>
-                {["all", ...BODY_PARTS].map((bodyPart) => (
-                  <CommandItem
-                    key={`bodyPart-${bodyPart}`}
-                    className="capitalize"
-                    onSelect={(currentValue) => {
-                      setSelecteBodyPart(
-                        currentValue === selectedBodyPart ? "" : currentValue
-                      );
-                      setIsBodyPartComboboxOpen(false);
-                    }}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        selectedBodyPart === bodyPart
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
-                    {bodyPart}
-                  </CommandItem>
-                ))}
+                {[{ value: "all", label: "All" }, ...BODY_PARTS].map(
+                  (bodyPart) => (
+                    <CommandItem
+                      key={bodyPart.value}
+                      value={bodyPart.value}
+                      onSelect={(currentValue) => {
+                        setSelecteBodyPart(
+                          currentValue === selectedBodyPart ? "" : currentValue
+                        );
+                        setIsBodyPartComboboxOpen(false);
+                      }}
+                    >
+                      <Check
+                        className={cn(
+                          "mr-2 h-4 w-4",
+                          selectedBodyPart === bodyPart.value
+                            ? "opacity-100"
+                            : "opacity-0"
+                        )}
+                      />
+                      {bodyPart.label}
+                    </CommandItem>
+                  )
+                )}
               </CommandGroup>
             </Command>
           </PopoverContent>
@@ -120,39 +126,45 @@ function FilterComponents() {
               variant="outline"
               role="combobox"
               aria-expanded={isEquipmentComboboxOpen}
-              className="w-[200px] justify-between capitalize"
+              className="w-[200px] justify-between"
             >
-              {selectedEquipment ? selectedEquipment : "Select Equipment..."}
+              {selectedEquipment
+                ? [{ value: "all", label: "All" }, ...EXERCISE_EQUIPMENTS].find(
+                    (equipment) => equipment.value === selectedEquipment
+                  )?.label
+                : "Select Equipment..."}
               <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-[200px] p-0">
             <Command>
               <CommandInput placeholder="Search Equipment..." />
-              <CommandEmpty>Equipment wasn&apos;t found.</CommandEmpty>
+              <CommandEmpty>Equipment wasn&apos;t found</CommandEmpty>
               <CommandGroup>
-                {["all", ...EXERCISE_EQUIPMENT].map((equipment) => (
-                  <CommandItem
-                    key={`equipment-${equipment}`}
-                    className="capitalize"
-                    onSelect={(currentValue) => {
-                      setSelectedEquipment(
-                        currentValue === selectedEquipment ? "" : currentValue
-                      );
-                      setIsEquipmentComboboxOpen(false);
-                    }}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        selectedEquipment === equipment
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
-                    {equipment}
-                  </CommandItem>
-                ))}
+                {[{ value: "all", label: "All" }, ...EXERCISE_EQUIPMENTS].map(
+                  (equipment) => (
+                    <CommandItem
+                      key={equipment.value}
+                      value={equipment.value}
+                      onSelect={(currentValue) => {
+                        setSelectedEquipment(
+                          currentValue === selectedEquipment ? "" : currentValue
+                        );
+                        setIsEquipmentComboboxOpen(false);
+                      }}
+                    >
+                      <Check
+                        className={cn(
+                          "mr-2 h-4 w-4",
+                          selectedEquipment === equipment.value
+                            ? "opacity-100"
+                            : "opacity-0"
+                        )}
+                      />
+                      {equipment.label}
+                    </CommandItem>
+                  )
+                )}
               </CommandGroup>
             </Command>
           </PopoverContent>
